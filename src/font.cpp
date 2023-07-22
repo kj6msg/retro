2dfd4697dbee0807a81fe8049639309d2e54fad7
Ryan Clarke <rfclarke@alum.wpi.edu>
2023-07-21 21:12:12 -0400

    Added input parameter validation to font.

--- src/font.cpp
+++ src/font.cpp
@@ -11,6 +11,7 @@
 #include <iterator>
 #include <ranges>
 #include <span>
+#include <stdexcept>
 #include <utility>
 #include <vector>
 
@@ -23,6 +24,11 @@ namespace retro
 font::font(const std::span<const std::byte> glyphs, const int width, const int height)
     : m_width{width}, m_height{height}, m_glyphs(glyphs.size())
 {
+    if(width < 1 || height < 1)
+    {
+        throw std::invalid_argument("font ctor has an invalid argument");
+    }
+
     std::ranges::copy(glyphs, m_glyphs.begin());
 }
 
@@ -30,6 +36,11 @@ font::font(const std::span<const std::byte> glyphs, const int width, const int h
 ////////////////////////////////////////////////////////////////////////////////
 std::vector<int> font::glyph(const unsigned char c, const int fg, const int bg) const
 {
+    if(fg < 0 || bg < 0)
+    {
+        throw std::invalid_argument("font::glyph has an invalid argument");
+    }
+    
     const auto glyph = m_glyphs | std::views::drop(m_height * c) | std::views::take(m_height);
     std::vector<int> color_glyph;
 
